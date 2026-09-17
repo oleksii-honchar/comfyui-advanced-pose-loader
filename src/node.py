3ce96d4dbd3c0ae2e9668b56c57855f3599bfc06
Oleksii Honchar <tuiteraz@gmail.com>
2026-09-17 20:55:18 +0200

    Fix: change vae and control_net to STRING inputs

--- src/node.py
+++ src/node.py
@@ -83,13 +83,11 @@ class AdvancedOpenposeLoader:
             "required": {
                 "model": ("MODEL", {}),
                 "conditioning": ("CONDITIONING", {}),
-                "vae": ("COMBO", {
-                    "values": vae_options,
-                    "default": vae_default,
+                "vae": ("STRING", {
+                    "default": "flux2-vae.safetensors",
                 }),
-                "control_net": ("COMBO", {
-                    "values": cn_options,
-                    "default": cn_default,
+                "control_net": ("STRING", {
+                    "default": "FLUX.2-dev-Fun-Controlnet-Union-2602-fp8.safetensors",
                 }),
                 "pose_folder_name": ("STRING", {
                     "default": "",
